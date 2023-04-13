982b96e81febd6e624f2e4b86ee750a635c90f9f
DOT503-DevOps <128017331+DOT503-DevOps@users.noreply.github.com>
2023-04-13 18:29:07 +0930

    Added the Java source code and pom.xml maven file

---
+++ src/main/java/DevOps_Maven/SimpleCalculator.java
@@ -0,0 +1,25 @@
+package DevOps_Maven;
+
+public class SimpleCalculator {
+
+    public int addition(int firstNumber, int secondNumber) {
+    		return firstNumber + secondNumber;
+    }
+    
+    public int subtraction(int firstNumber, int secondNumber) {
+		return firstNumber - secondNumber; 
+	}
+    
+    public int multiplication(int firstNumber, int secondNumber) {
+		return firstNumber * secondNumber; 
+	}
+    
+    public int modulo(int firstNumber, int secondNumber) {
+		return firstNumber % secondNumber; 
+	}
+    
+    public int square(int number) {
+		return number * number; 
+	}
+
+}
